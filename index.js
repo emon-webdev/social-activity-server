493db815e17edef65f9134bce198ddd6bca8935f
emon-webdev <emon.webdev@gmail.com>
2022-12-29 00:30:20 +0600

    post api, detail pai

--- index.js
+++ index.js
@@ -1,65 +1,85 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 const app = express();
-require("dotenv").config();
-
-const Port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
+//middleware
 app.use(cors());
 app.use(express.json());
-const allPost = require("./data/posts.json");
-// social-activity
-// SfDTW2TI8YieaW9D
 
-// m3M3akVwvFBW97bp
-
-// const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.gtaii29.mongodb.net/?retryWrites=true&w=majority`;
-// const uri = `mongodb+srv://social-activity:Kj90XADaigRbeSZa@cluster0.gtaii29.mongodb.net/?retryWrites=true&w=majority`;
-
-const uri =
-  "mongodb+srv://social-activity:Kj90XADaigRbeSZa@cluster0.gtaii29.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://socialActivitis:LEx304HZEg1scxBl@cluster0.nxgxqoq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
-console.log(uri);
 async function run() {
   try {
-    //database collection
-    const postCollection = client.db("social-activity").collection("posts");
+    const postCollection = client.db("social-activitis").collection("posts");
 
+    //send post in db
     app.post("/posts", async (req, res) => {
       const post = req.body;
       const result = await postCollection.insertOne(post);
-      console.log(post, result);
+      console.log(result);
       res.send(result);
     });
+    //
+    app.get("/posts", async (req, res) => {
+      const query = {};
+      const result = await postCollection
+        .find(query, {sort:{_id:-1}})
+        .toArray();
+      res.send(result);
+    });
+
+    //single post for service details com
+    app.get('/posts/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await postCollection.findOne(query);
+      res.send(result)
+    })
+
+    //
+    // app.get("/services/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const service = await servicesCollection.findOne(query);
+    //   res.send(service);
+    // });
+
+    //all services
+    // app.get("/services", async (req, res) => {
+    //   const query = {};
+    //   const cursor = servicesCollection.find(query, {sort:{_id:-1}});
+    //   const services = await cursor.toArray();
+    //   res.send(services);
+    // });
   } finally {
   }
 }
-run().catch(console.dir);
+run().catch((error) => console.log(error));
 
-
-app.get('/posts', (req, res) => {
-  res.send(allPost)
-})
-
-app.get('/posts/:id', (req, res) => {
-  const id = req.params.id;
-  const result = allPost.find(post => post.id == id);
-  res.send(result)
-})
-
-
-
+console.log(uri);
 
 app.get("/", (req, res) => {
-  res.send("Social Activity Server is Running");
+  res.send("Social activity database");
 });
 
-app.listen(Port, () => {
-  console.log("Social Activity is running", Port);
+app.listen(port, () => {
+  console.log(`social activity database running ${port}`);
 });
+
+// const allPost = require("./data/posts.json");
+
+// app.get("/posts", (req, res) => {
+//   res.send(allPost);
+// });
+
+// app.get("/post-details/:id", (req, res) => {
+//   const id = req.params.id;
+//   const result = allPost.find((post) => post.id == id);
+//   res.send(result);
+// });
